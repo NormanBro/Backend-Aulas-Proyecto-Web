12dcf566887818fc6e724babd0789fc1845e46e4
NormanBro <88945718+NormanBro@users.noreply.github.com>
2022-10-09 23:35:12 -0600

    La raiz

--- src/app.js
+++ src/app.js
@@ -2,6 +2,10 @@ const app= require('./setting/index');
 
 const puerto=process.env.PORT || app.get("PORT");
 
+app.get('/',(res,req)=>{
+    return "Hola mundo";
+})
+
 app.listen(puerto,()=>{
     console.log("localhost:"+puerto);
 })
